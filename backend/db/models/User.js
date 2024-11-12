4e915e575f027ca03852551ba2c35e9f01fb481a
Ioana Grigoriu <92142802+GIoana02@users.noreply.github.com>
2024-11-12 15:18:16 +0200

    Starting implementing RestAPI endpoints

---
+++ backend/db/models/User.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./database');
+
+const User = sequelize.define('User', {
+  user_id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password_hash: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  two_factor_enabled: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+});
+
+module.exports = User;
